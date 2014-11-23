d289daa70a1fba9ed4d0e8431fc43883ac0ef44f
emeasee <hello@m-jo.co>
2014-11-23 20:13:30 +0000

    todos try 3

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('browser-sync', function () {
       }
    });
 });
-// TODO: Add a deploy to gh-pages option for the short term. To serve before cloud setup
+//TODO: Add a deploy to gh-pages option for the short term. To serve before cloud setup [Issue: https://github.com/emeasee/piment-hbp5/issues/2]
 gulp.task('build', ['clean'], function(){
     gulp.start('usemin');
 });
